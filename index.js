d429eb6c512c35385c125c9cc0e9a80ed76010fb
27Raksha <128985351+27Raksha@users.noreply.github.com>
2023-06-08 22:27:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var random1= Math.floor(Math.random()*6)+1;
 var newdice1="dice"+random1+".png";
-var newimage1="images/"+newdice1;
 var image1= document.querySelectorAll("img")[0];
-image1.setAttribute("src",newimage1);
+image1.setAttribute("src",newdice1);
 var random2= Math.floor(Math.random()*6)+1;
 var newdice2="dice"+random2+".png";
-var newimage2="images/"+newdice2;
 var image2= document.querySelectorAll("img")[1];
-image2.setAttribute("src",newimage2);
+image2.setAttribute("src",newdice2);
 if(random1>random2){
     document.querySelector("h1").innerHTML("🚩Player 1 win!");
 }
